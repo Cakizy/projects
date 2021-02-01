94e036fa5066b4a5fb85bd418b9580ce0598ed7a
Stefan <stefanhadzieftimov@gmail.com>
2021-02-01 14:12:11 +0100

    Break infinit loop

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ async function getQuote() {
         quoteText.innerText = data.quoteText;
         removeLoadingSpinners();
     } catch (error) {
-        getQuote();
+        // getQuote();
     }
 }
 // Tweet Quote
